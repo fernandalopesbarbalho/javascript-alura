f71dc9bb21c50089f3238eff6ba76a5a5819b657
Fernanda Lopes Barbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2023-07-26 15:13:32 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//Quadrado verde no canto inferior direito é mostrado caso o mouse não estiver pressionado e, caso estiver, muda para um vermelho no canto superior esquerdo.
+
+function setup() {
+  createCanvas(200, 200);
+
+}
+
+function draw() {
+  background("white");
+  
+  noStroke();
+
+  if (mouseIsPressed) {
+
+    fill("red");
+    rect(0, 0, 100, 100);
+
+  } else {
+    fill("green");
+    rect(100, 100, 100, 100);
+
+  }
+
+}
